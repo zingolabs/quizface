faf00a938b82ace9f218f12d8940db68d12cdd61
dannasessha <sessha@zingolabs.com>
2021-03-14 00:51:08 +0000

    add mundane and cursed command text files, add mundane to processed commands in main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,15 @@ fn main() {
 
     let commands = ingest_commands();
 
-    // make blessed tome
+    // make blessed and mundane tomes
     // assumes blessed --bin has previously been run
     // or blessed.txt has checked in
-    let location = format!(
+    // and mundane.txt is checked in
+    let blessed_location = format!(
         "./logs/{}/blessed_commands/blessed.txt",
         create_version_name()
     );
-    let blessed_path = std::path::Path::new(&location);
+    let blessed_path = std::path::Path::new(&blessed_location);
     let blessed_reader = std::io::BufReader::new(
         std::fs::File::open(blessed_path).expect("Alas! No blessed text!"),
     );
@@ -24,17 +25,24 @@ fn main() {
         .map(|l| l.expect("Could not make line."))
         .collect();
 
+    let mundane_location = "./lists/mundate.txt";
+    let mundane_path = std::path::Path::new(&mundane_location);
+    let mundane_reader = std::io::BufReader::new(
+        std::fs::File::open(mundane_path).expect("No mundane text!"),
+    );
+    let mundane_tome: Vec<String> = mundane_reader
+        .lines()
+        .map(|l| l.expect("Could not make line."))
+        .collect();
+
     for command in commands {
         let command_help_output = get_command_help(&command);
-
         check_success(&command_help_output.status);
-
-        let raw_command_help = std::str::from_utf8(&command_help_output.stdout)
-            .expect("Invalid raw_command_help, error!");
-
+        let raw_command_help = std::str::from_utf8(&command_help_output.stdout).expect("Invalid raw_command_help, error!");
         log_raw_output(&command, raw_command_help.to_string());
-        //select just for blessed results.
-        if blessed_tome.contains(&command) {
+
+        //select just for blessed and mundane results.
+        if blessed_tome.contains(&command) || mundane_tome.contains(&command) {
             //dbg!(&command);
             produce_interpretation(raw_command_help);
         }
