b1a3b660f1b3e5e5bd9e8eccbf269df3b7e3900c
dannasessha <sessha@zingolabs.com>
2021-03-15 19:57:46 +0000

    first prescrubbing.rs logic complete, adjusted lists in mundane.txt and cursed.txt

--- src/utils/prescrubbing.rs
+++ src/utils/prescrubbing.rs
@@ -1,6 +1,6 @@
 pub fn prescrub(command: &str, raw_command_help: &str) -> String {
     match command {
-        "importaddress" | "importpubkey" | "encryptwallet" | "addnode" | "disconnectnode" | "importprivkey" | "importwallet" | "setlogfilter" => {
+        "importaddress" | "importpubkey" | "encryptwallet" | "addnode" | "disconnectnode" | "importprivkey" | "importwallet" | "setlogfilter" | "setban" | "keypoolrefill" | "z_importwallet" | "clearbanned" |  "setaccount" | "setgenerate" => {
             raw_command_help.replace("Examples:", "Result: \nExamples:")
         }
         "settxfee" | "getgenerate" | "generate" => { 
@@ -9,24 +9,25 @@ pub fn prescrub(command: &str, raw_command_help: &str) -> String {
         "help" => {
             raw_command_help.replace("The help text","The help text \n Examples:")
         } 
+        //TODO why is this not blessed/this is a scrub.
         "estimatepriority" => {
             raw_command_help.replace("n :", r#""n :""#)       
         }
         "getunconfirmedbalance" => {
             raw_command_help.replace("unconfirmed balance", r#"unconfirmed balance \n Result: "numberic""#)       
         } 
-        "createrawtransaction" | "getrawmempool" => {
+        "createrawtransaction" => {
             raw_command_help.replace("Examples", "Examples:")       
         }
         "prioritisetransaction" => {
             raw_command_help.replace(r#"Result
 true"#, r#"Result: \n "true""#)
         }
-    "z_getmigrationstatus" => {
-        raw_command_help.replace("}", "}\nExamples:\n")       
+        "z_getmigrationstatus" | "zcrawreceive" => {
+            raw_command_help.replace("}", "}\nExamples:\n")       
         } 
-    "zcrawjoinsplit" => {
-        raw_command_help.replace(r#"Output: {
+        "zcrawjoinsplit" => {
+            raw_command_help.replace(r#"Output: {
   "encryptednote1": enc1,
   "encryptednote2": enc2,
   "rawtxn": rawtxout
@@ -34,66 +35,44 @@ true"#, r#"Result: \n "true""#)
   "encryptednote1": enc1,
   "encryptednote2": enc2,
   "rawtxn": rawtxout
-}\nExamples:\n"#)       
-    }
-        _ => { 
-            "to".to_string()
+Examples:
+"#)       
+        }
+        "z_listunspent" | "getbestblockhash" | "listunspent" => {
+            let intermediate_command_help = raw_command_help.replace("Result", "Result:");
+            intermediate_command_help.as_str().replace("Examples", "Examples:")
+        }
+        "zcrawkeygen" => {
+            let intermediate_command_help = raw_command_help.replace("Output:", "Result:");
+            intermediate_command_help.as_str().replace("}", r#"}
+            Examples:
+            "#)
+        }
+        "stop" => {
+            raw_command_help.replace(r#"stop"#, r#"stop
+                                 Result:
+                                 Examples:
+                                 "#)       
+        } 
+        "gettxoutproof" => {
+            raw_command_help.replace(r#"proof."#, r#"proof.
+                                 Examples:
+                                 "#)       
+        } 
+        "verifytxoutproof" => {
+            raw_command_help.replace(r#"proof is invalid"#, r#"proof is invalid
+        Examples:
+        "#)       
+        } 
+        //TODO why is this not blessed? same as estimatepriority
+        "estimatefee" => {
+            raw_command_help.replace("", "")       
+        } 
+        "zcbenchmark" => {
+            raw_command_help.replace("", "")       
+        } 
+        _ => {
+            panic!("unexpected mundane command");
         }
     }
 }
-    /*
-    "z_listunspent" => {
-        raw_command_help.replace("", "")       
-    } else if command == "
-zcrawreceive
-        raw_command_help.replace("", "")       
-    } else if command == "
-getbestblockhash
-        raw_command_help.replace("", "")       
-    } else if command == "
-zcrawkeygen
-        raw_command_help.replace("", "")       
-    } else if command == "
-stop
-        raw_command_help.replace("", "")       
-    } else if command == "
-setban
-        raw_command_help.replace("", "")       
-    } else if command == "
-keypoolrefill
-        raw_command_help.replace("", "")       
-    } else if command == "
-gettxoutproof
-        raw_command_help.replace("", "")       
-    } else if command == "
-getaddressutxos
-        raw_command_help.replace("", "")       
-    } else if command == "
-verifytxoutproof
-        raw_command_help.replace("", "")       
-    } else if command == "
-estimatefee
-        raw_command_help.replace("", "")       
-    } else if command == "
-z_importwallet
-        raw_command_help.replace("", "")       
-    } else if command == "
-getblockheader
-        raw_command_help.replace("", "")       
-    } else if command == "
-listunspent
-        raw_command_help.replace("", "")       
-    } else if command == "
-clearbanned
-        raw_command_help.replace("", "")       
-    } else if command == "
-setaccount
-        raw_command_help.replace("", "")       
-    } else if command == "
-setgenerate
-        raw_command_help.replace("", "")       
-    } else if command == "
-zcbenchmark
-}
-*/
-
