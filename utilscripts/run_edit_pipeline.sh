5bf3fee61b8cba190b97040d2a8ab6aab1daaece
zancas <zancas@zingolabs.com>
2021-04-19 14:19:47 -0600

    rm utilscripts/run_edit_pipeline

--- utilscripts/run_edit_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-set -x
-ZCASHROOT=${HOME}/src/zcash
-QUIZFACEROOT=${HOME}/src/quizface
-ZCASHRPCROOT=${HOME}/src/zcashrpc
-TYPEGENROOT=${ZCASHRPCROOT}/zcashrpc-typegen
-
-source $QUIZFACEROOT/utilscripts/pipeline_for_doc_edits.sh
