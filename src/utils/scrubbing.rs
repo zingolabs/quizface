22df4c03fcfa45c6428046bdf6b780a6f50366a9
dannasessha <sessha@zingolabs.com>
2021-04-20 22:17:48 +0000

    removed z_mergetoaddress scrubbing

--- src/utils/scrubbing.rs
+++ src/utils/scrubbing.rs
@@ -1,30 +1,8 @@
-macro_rules! args_fromaddresses_array {
-    ($arguments_data:expr) => {
-        $arguments_data.replace(r#"(array, required) A JSON array with addresses.
-                         The following special strings are accepted inside the array:
-                             - "ANY_TADDR":   Merge UTXOs from any taddrs belonging to the wallet.
-                             - "ANY_SPROUT":  Merge notes from any Sprout zaddrs belonging to the wallet.
-                             - "ANY_SAPLING": Merge notes from any Sapling zaddrs belonging to the wallet.
-                         While it is possible to use a variety of different combinations of addresses and the above values,
-                         it is not possible to send funds from both sprout and sapling addresses simultaneously. If a special
-                         string is given, any given addresses of that type will be counted as duplicates and cause an error.
-    [
-      "address"          (string) Can be a taddr or a zaddr
-      ,...
-    ]"#, r#"[
-    "address"          (string) Can be a taddr or a zaddr
-    ]"#)
-    };
-}
-
 pub(crate) fn scrub_arguments(
     rpc_name: &str,
     arguments_data: String,
 ) -> String {
     match rpc_name {
-        "z_mergetoaddress" => {
-            args_fromaddresses_array!(arguments_data)
-        }
         _ => arguments_data,
     }
 }
