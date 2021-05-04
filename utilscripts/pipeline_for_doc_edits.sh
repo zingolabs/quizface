26ce0c963b76ed85acd4a1525a2d96d90a28568d
Hazel OHearn <gygaxis@zingolabs.org>
2021-05-04 16:03:38 -0300

    added removal of stale output to pipeline

--- utilscripts/pipeline_for_doc_edits.sh
+++ utilscripts/pipeline_for_doc_edits.sh
@@ -12,7 +12,9 @@ time make -j$(nproc)
 # 2.5 seconds appears to be close to the minimum necessary boot time
 sleep 2.5
 cd $QUIZFACEROOT
-cargo build -q && cargo doc -q && cargo test -q && cat $QUIZFACEROOT/lists/passing.txt | PATH=$PATH:$ZCASHROOT/src xargs cargo run -q
+cargo build -q && cargo doc -q && cargo test -q 
+rm -r output/metascrubbing_0.2.0
+cat $QUIZFACEROOT/lists/passing.txt | PATH=$PATH:$ZCASHROOT/src xargs cargo run -q
 QUIZFOUT=$QUIZFACEROOT/output/`ls -1rct $QUIZFACEROOT/output/ | tail -n 1`
 cd $ZCASHRPCROOT && cargo test -q --workspace
 cd $TYPEGENROOT && cargo run -q $QUIZFOUT
