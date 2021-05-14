42105197e131b72767ce5307800ae636b65243bb
dannasessha <sessha@zingolabs.com>
2021-05-14 18:29:00 +0000

    actually deleted keypoolrefill from prescrubbing

--- src/utils/prescrubbing.rs
+++ src/utils/prescrubbing.rs
@@ -9,9 +9,6 @@ pub fn prescrub(rpc_name: &str, raw_rpc_name_help: &str) -> String {
             .unwrap()
             .replace_all(raw_rpc_name_help, "Result:")
             .to_string(),
-        /*"keypoolrefill" => raw_rpc_name_help
-        .replace("Examples:", "Result:\nExamples:")
-        .replace("Arguments", "Arguments:"),*/
         "settxfee" => raw_rpc_name_help.replace("Result", "Result:"),
         _ => raw_rpc_name_help.to_string(),
     }
