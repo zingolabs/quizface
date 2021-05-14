9b90af7a5879db011177d0780aa087239503cc65
zancas <zancas@zingolabs.org>
2021-05-14 11:22:07 -0600

    remove z_getmigrationstatus from prescrub

--- src/utils/prescrubbing.rs
+++ src/utils/prescrubbing.rs
@@ -12,19 +12,12 @@ pub fn prescrub(rpc_name: &str, raw_rpc_name_help: &str) -> String {
             .unwrap()
             .replace_all(raw_rpc_name_help, "Result:")
             .to_string(),
-        "keypoolrefill" => {
-            raw_rpc_name_help.replace("Examples:", "Result:\nExamples:")
-                .replace("Arguments", "Arguments:")
-        }
-        "settxfee" => {
-            raw_rpc_name_help.replace("Result", "Result:")
-        }
+        "keypoolrefill" => raw_rpc_name_help
+            .replace("Examples:", "Result:\nExamples:")
+            .replace("Arguments", "Arguments:"),
+        "settxfee" => raw_rpc_name_help.replace("Result", "Result:"),
         "estimatepriority" | "estimatefee" => {
             raw_rpc_name_help.replace("Example:", "Examples:")
-        }
-        "z_getmigrationstatus" => {
-            raw_rpc_name_help.replace("}", "}\nExamples:\n")
-                .replace(r#""migration_txids": [txids]                (json array of strings) An array of all migration txids involving this wallet"#, "\"migration_txids\": [\n \"txids\"  (string) An array of all migration txids involving this wallet\n]")
         }
         _ => raw_rpc_name_help.to_string(),
     }
